5e6878ccd85d75bdea224d7918b88cb9bbd25bf6
Kevin Ukota <z1ppm4n@gmail.com>
2024-08-11 21:23:07 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Restaurant Page",
+      filename: "index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
